8fcf9f80f4a28a82bb19bd3416c0d48c1965c497
Roel Magdaleno <roelmagdaleno@gmail.com>
2021-04-26 22:09:38 -0500

    IMPROVE: Finish AJAX functionality with tests

--- includes/class-wp-phpunit-pe-ajax.php
+++ includes/class-wp-phpunit-pe-ajax.php
@@ -17,16 +17,17 @@ class WP_PHPUnit_PE_AJAX {
 		check_ajax_referer( 'wp_phpunit_ajax_update_email' );
 
 		$data = json_decode( wp_unslash( $_POST['data'] ), true );
+		$user = $data['user'];
 
-		if ( empty( $data ) ) {
+		if ( empty( $user ) || ! isset( $user['email'] ) ) {
 			wp_send_json_error( array( 'message' => 'There is no data to save.' ) );
 		}
 
-		if ( ! is_email( $data['email'] ) ) {
+		if ( ! is_email( $user['email'] ) ) {
 			wp_send_json_error( array( 'message' => 'The inserted email is invalid.' ) );
 		}
 
-		update_option( 'wp_phpunit_tests_option', $data );
+		update_option( 'wp_phpunit_tests_option', $user );
 		wp_send_json_success( array( 'message' => 'Option updated successfully.' ) );
 	}
 }
--- tests/test-class-wp-phpunit-pe-ajax.php
+++ tests/test-class-wp-phpunit-pe-ajax.php
@@ -4,13 +4,19 @@ class WP_PHPUnit_PE_Ajax_Test extends WP_Ajax_UnitTestCase {
 	private $nonce;
 
 	public function setUp() {
+		/**
+		 * Always call parent::setUp to avoid policy errors.
+		 */
+		parent::setUp();
+		$this->_setRole( 'administrator' );
+
 		$this->nonce = wp_create_nonce( 'wp_phpunit_ajax_update_email' );
 		new WP_PHPUnit_PE_AJAX();
 	}
 
 	public function test_if_sent_data_is_empty() {
 		$_POST['_wpnonce'] = $this->nonce;
-		$_POST['data']     = wp_json_encode( array( 'data' => array() ) );
+		$_POST['data']     = wp_json_encode( array( 'user' => array() ) );
 
 		try {
 			$this->_handleAjax( 'wp_phpunit_update_email' );
@@ -20,14 +26,14 @@ class WP_PHPUnit_PE_Ajax_Test extends WP_Ajax_UnitTestCase {
 
 		$response = json_decode( $this->_last_response, true );
 
-		$this->assertSame( 'error', $response['type'] );
-		$this->assertSame( 'There is no data to save.', $response['message'] );
+		$this->assertFalse( $response['success'] );
+		$this->assertSame( 'There is no data to save.', $response['data']['message'] );
 	}
 
 	public function test_if_email_to_update_is_invalid() {
 		$_POST['_wpnonce'] = $this->nonce;
 		$_POST['data']     = wp_json_encode( array(
-			'data' => array( 'email' => 'hola.org' ),
+			'user' => array( 'email' => 'hola.org' ),
 		) );
 
 		try {
@@ -38,14 +44,15 @@ class WP_PHPUnit_PE_Ajax_Test extends WP_Ajax_UnitTestCase {
 
 		$response = json_decode( $this->_last_response, true );
 
-		$this->assertSame( 'error', $response['type'] );
-		$this->assertSame( 'The inserted email is invalid.', $response['message'] );
+		$this->assertFalse( $response['success'] );
+		$this->assertSame( 'The inserted email is invalid.', $response['data']['message'] );
 	}
 
 	public function test_update_email_success() {
+		$valid_email       = 'hola@example.org';
 		$_POST['_wpnonce'] = $this->nonce;
 		$_POST['data']     = wp_json_encode( array(
-			'data' => array( 'email' => 'hola@example.org' ),
+			'user' => array( 'email' => $valid_email ),
 		) );
 
 		try {
@@ -54,9 +61,13 @@ class WP_PHPUnit_PE_Ajax_Test extends WP_Ajax_UnitTestCase {
 
 		$this->assertTrue( isset( $e ) );
 
-		$response = json_decode( $this->_last_response, true );
+		$response  = json_decode( $this->_last_response, true );
+		$user_data = get_option( 'wp_phpunit_tests_option', array() );
 
-		$this->assertSame( 'success', $response['type'] );
-		$this->assertSame( 'Option updated successfully.', $response['message'] );
+		$this->assertNotEmpty( $user_data );
+		$this->assertArrayHasKey( 'email', $user_data );
+		$this->assertSame( $valid_email, $user_data['email'] );
+		$this->assertTrue( $response['success'] );
+		$this->assertSame( 'Option updated successfully.', $response['data']['message'] );
 	}
 }
